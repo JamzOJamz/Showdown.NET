d42bab8a0683500fab7b9a1cc4e6c215c485b8d1
JamzOJamz <abstractdeveloperx@gmail.com>
2025-09-18 19:18:40 -0600

    Make BattleStream disposable

--- Showdown.NET/Simulator/BattleStream.cs
+++ Showdown.NET/Simulator/BattleStream.cs
@@ -4,9 +4,10 @@ using Microsoft.ClearScript;
 namespace Showdown.NET.Simulator;
 
 [PublicAPI]
-public class BattleStream
+public class BattleStream : IDisposable
 {
     private readonly dynamic _wrappedBattleStream;
+    private bool _disposed = false;
 
     public BattleStream()
     {
@@ -19,11 +20,13 @@ public class BattleStream
 
     public void Write(string command)
     {
+        ThrowIfDisposed();
         _wrappedBattleStream.write(command);
     }
     
     public async Task<string> ReadAsync()
     {
+        ThrowIfDisposed();
         var engine = ShowdownHost.Engine!;
         try
         {
@@ -45,11 +48,43 @@ public class BattleStream
     
     public async IAsyncEnumerable<string> ReadOutputsAsync()
     {
+        ThrowIfDisposed();
         while (true)
         {
             var output = await ReadAsync();
             if (output == null) yield break;
             yield return output;
+        }
+    }
+    
+    private void ThrowIfDisposed()
+    {
+        if (!_disposed) return;
+        throw new ObjectDisposedException(nameof(BattleStream));
+    }
+    
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed && disposing)
+        {
+            try
+            {
+                if (_wrappedBattleStream is ScriptObject so) {
+                    Console.WriteLine("Freeing script object");
+                    so.Dispose();
+                }
+            }
+            catch
+            {
+                // Ignore errors during disposal
+            }
+            _disposed = true;
         }
     }
 }
